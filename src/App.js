0cc43116f278c269aa3d7d411f0eb0dd0698902a
albeartoz <zhang.alb@northeastern.edu>
2021-02-20 18:01:12 -0500

    add cross-fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import logo from './covidex-logo.png';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
+import fetch from 'cross-fetch';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
   return (
@@ -13,8 +16,77 @@ function App() {
         <Form className="App-form">
           <Form.Control className="text-center" size="lg" type="text" placeholder="Type your location here" />
         </Form>
+        <Asynchronous/>
       </header>
     </div>
+  );
+}
+
+function Asynchronous() {
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      const response = await fetch('https://country.register.gov.uk/records.json?page-size=5000');
+      await sleep(1e3); // For demo purposes.
+      const countries = await response.json();
+
+      if (active) {
+        setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      style={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
+      options={options}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Asynchronous"
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
   );
 }
 
