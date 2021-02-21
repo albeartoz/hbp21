f0754b82f643dd22cc8803251782c27ab3e1dc53
albeartoz <zhang.alb@northeastern.edu>
2021-02-20 22:01:53 -0500

    add cities json

--- src/App.js
+++ src/App.js
@@ -8,86 +8,91 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Form className="App-form">
+        {/* <Form className="App-form">
           <Form.Control className="text-center" size="lg" type="text" placeholder="Type your location here" />
-        </Form>
-        <Asynchronous/>
-      </header>
+        </Form> */}
+        <FreeSolo/>
     </div>
   );
 }
 
-function Asynchronous() {
-  const [open, setOpen] = React.useState(false);
-  const [options, setOptions] = React.useState([]);
-  const loading = open && options.length === 0;
-
-  React.useEffect(() => {
-    let active = true;
-
-    if (!loading) {
-      return undefined;
-    }
-
-    (async () => {
-      const response = await fetch('https://country.register.gov.uk/records.json?page-size=5000');
-      await sleep(1e3); // For demo purposes.
-      const countries = await response.json();
-
-      if (active) {
-        setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
-      }
-    })();
-
-    return () => {
-      active = false;
-    };
-  }, [loading]);
-
-  React.useEffect(() => {
-    if (!open) {
-      setOptions([]);
-    }
-  }, [open]);
-
+function FreeSolo() {
   return (
-    <Autocomplete
-      id="asynchronous-demo"
-      style={{ width: 300 }}
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      getOptionSelected={(option, value) => option.name === value.name}
-      getOptionLabel={(option) => option.name}
-      options={options}
-      loading={loading}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Asynchronous"
-          variant="outlined"
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
-          }}
-        />
-      )}
-    />
+    <div className="App-form">
+      <Autocomplete 
+        className="text-center"
+        id="city-search"
+        options={locations.map((option) => option.name)}
+        renderInput={(params) => (
+          <TextField {...params} label="Search for your city" margin="normal" variant="filled" />
+        )}
+      />
+    </div>
   );
 }
+
+const locations = [
+  {
+    "name" : "New York, New York",
+    "latitude": 40.7,
+    "longitude" : -73.9
+  },
+  {
+    "name": "Los Angeles, California",
+    "latitude": 34.1,
+    "longitude" : -118.2
+  },
+  {
+    "name" : "Boston, Massachusetts",
+    "latitude": 42.3,
+    "longitude" : -71.1
+  },
+  {
+    "name" : "Chicago, Illinois",
+    "latitude": 41.9,
+    "longitude" : -87.6
+  },
+  {
+    "name" : "Houston, Texas",
+    "latitude": 29.7,
+    "longitude" : -95.4
+  },
+  {
+    "name" : "Phoenix, Arizona",
+    "latitude": 33.6,
+    "longitude" : -112.1
+  },
+  {
+    "name" : "Philadelphia, Pennsylvania",
+    "latitude": 40,
+    "longitude" : -75.1
+  },
+  {
+    "name" : "San Antonio, Texas",
+    "latitude": 29.5,
+    "longitude" : -98.5
+  },
+  {
+    "name" : "San Diego, California",
+    "latitude": 32.9,
+    "longitude" : -117.1
+  },
+  {
+    "name" : "Columbus, Ohio",
+    "latitude": 40,
+    "longitude" : -83
+  },
+  {
+    "name" : "Seattle, Washington",
+    "latitude": 47.6,
+    "longitude" : -112.4
+  },
+];
 
 export default App;
